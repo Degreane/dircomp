627b31f413ef361b44256b659f5c635c2f9e5974
degreane <degreane@gmail.com>
2019-06-22 11:26:50 +0300

    Added XML Functionality and Duplicates

--- dircomp.py
+++ dircomp.py
@@ -3,6 +3,10 @@ from PyQt5.QtWidgets import QFileDialog, QMessageBox
 from DirShaComp import Ui_FileReplicants
 import os
 from lxml import etree
+from hashlib import md5
+
+
+BufferSize = 1024
 
 
 class qtapp(Ui_FileReplicants):
@@ -24,15 +28,67 @@ class qtapp(Ui_FileReplicants):
         self.BtnAnalysis.clicked.connect(self.analyseWorkingDir)
         self.BtnClearDebug.clicked.connect(self.ProcessDebug.clear)
 
+    def readfileHash(self, file2read):
+        myhash = md5()
+        with open(file2read, "rb") as fd:
+            bytesRead = fd.read(BufferSize)
+            while len(bytesRead) > 0:
+                myhash.update(bytesRead)
+                bytesRead = fd.read(BufferSize)
+            return myhash.hexdigest()
+
     def analyseWorkingDir(self):
         for root, dirs, files in os.walk(self.currentDir):
-            rowMsg = "{0} consumes [{1}] [{2}]\n".format(root, dirs, files)
+            rowMsg = "{0}<-root \n\t[{1}] <-dirs\n\t\t[{2}] \n".format(
+                root,
+                dirs,
+                files
+            )
             self.ProcessDebug.appendPlainText(rowMsg)
+            # print(rowMsg)
+            """
+                Next we shall be appending the file to the XML
+            """
+            for thefile in files:
+                file2read = os.path.join(root, thefile)
+                hashedFile = self.readfileHash(file2read)
+                self.ProcessDebug.appendPlainText(
+                    "Hashed = {0}".format(
+                        hashedFile
+                    )
+                )
+                """
+                Next Search for hashed File if found in document
+                if found in document then append the data to it
+                else create a new node and append it
+                """
+                hashedElement = self.Tree.find("h_{0}".format(hashedFile))
+                if etree.iselement(hashedElement):
+                    # print(
+                    #     "Duplicate => {0} {1}".format(
+                    #         hashedElement.find('File').text,
+                    #         file2read
+                    #     )
+                    # )
+                    pass
+                else:
+                    hashedElement = etree.Element("h_{0}".format(hashedFile))
+                El = etree.Element('File')
+                El.text = file2read
+                hashedElement.append(El)
+                self.Tree.append(hashedElement)
+        # print(etree.tostring(self.Tree, encoding=str))
+        self.ProcessDebug.appendPlainText(
+            etree.tostring(
+                self.Tree,
+                encoding=str
+            )
+        )
 
     def changeWorkingDir(self, directory=None):
         # self.currentDir=
         print(type(directory))
-        if type(directory) != bool:
+        if not isinstance(directory, bool):
 
             if directory is not None and len(directory) != 0:
                 self.currentDir = directory
